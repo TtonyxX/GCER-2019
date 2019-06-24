efbea1e2a91ca7df73d54dcf2ef6fb65c439e8a2
RussellChien <36806880+RussellChien@users.noreply.github.com>
2019-06-23 22:10:57 -0700

    Update main.c

--- Code/main.c
+++ Code/main.c
@@ -1,12 +1,3 @@
-/*
-==========================================================================
-    Copyright 2019 All Rights Reserved - GCER 2019
-	Name:          Seeding Wallaby 
-	By:            Russell Chien
-
-	PROPRIETARY and CONFIDENTIAL
-============================================================================
-*/
 #include <kipr/botball.h>
 #include <drive.h>
 
@@ -29,6 +20,82 @@ int timeout; //if camera does not sense burning timeout will be set to 1
 #define black 3800
 #define white 2800
 #define grey 3600
+
+int gyrocalibrate() {
+    int changeGyroZ = 0;//find average gyro value when still
+    int i = 0;
+    for(i = 0; i < 5000; i++) {
+        changeGyroZ += gyro_z();
+    }
+    changeGyroZ /= 5000;
+
+    return changeGyroZ;
+}
+
+void move(int speed, int time, int changeGyroZ) {//speed -100 to 100, time is in miliseconds
+    int i = 0;
+    int anglechange = 0;
+    for(i = 0; i < time; i++) {//gyro move
+        anglechange += gyro_z()-changeGyroZ;
+        motor(MOT_LEFT, speed+anglechange/300);
+        motor(MOT_RIGHT, speed-anglechange/300);
+        msleep(1);
+    }
+    ao();
+}
+
+void turn(int direction, int degree, int changeGyroZ) {
+    int i = 0;
+    int anglechange = 0;
+    int x = degree*57000/90;
+    if(direction == 1) { // 1 = counterclockwise, 0 = clockwise
+        for(i = 0; i < 100; i++) {
+            motor(0, 100);
+            motor(1, -100);
+            msleep(1);
+        }
+        while(anglechange > -x) {
+            anglechange += gyro_z()-changeGyroZ;
+            motor(0, 100);
+            motor(1, -100);
+            msleep(1);
+        }
+    } else {
+        for(i = 0; i < 100; i++) {
+            motor(0, -100);
+            motor(1, 100);
+            msleep(1);
+        }
+        while(anglechange < x) {
+            anglechange += gyro_z()-changeGyroZ;
+            motor(0, -100);
+            motor(1, 100);
+            msleep(1);
+        }
+    }
+    ao();
+}
+
+void turn90(int direction, int changeGyroZ) {
+    int anglechange = 0;
+    int x = 90*170000/90;
+    if(direction == 1) { // 1 = left, 0 = right
+        while(anglechange > -x) {
+            anglechange += gyro_z()-changeGyroZ;
+            motor(0, 100);
+            motor(1, -100);
+            msleep(1);
+        }
+    } else {
+        while(anglechange < x) {
+            anglechange += gyro_z()-changeGyroZ;
+            motor(0, -100);
+            motor(1, 100);
+            msleep(1);
+        }
+    }
+    ao();
+}
 
 void updateCam(){
     camData[1] = camera_update();
@@ -102,13 +169,13 @@ void scanBuilding(){
         mav(MOT_LEFT, 950);
         mav(MOT_RIGHT, 900);
         msleep(100);
-        if(gmpc(MOT_LEFT) > 2000){
+        if(gmpc(MOT_LEFT) > 4200){
             printf("scan time out\n");
-            timeout++;
+            timeout = 1;
         }
     }
     ao();
-    if(gmpc(MOT_LEFT) < 2000){
+    if(gmpc(MOT_LEFT) < 3200){
         burning_building = 0; 
         printf("burning building is close\n");
     }
@@ -149,23 +216,27 @@ void line_sense(int speed) {
             drive(speed, speed);
         msleep(1);
     }
+    printf("line sensed\n");
     ao();
 }
+
+
 
 void line_sense_grey(int speed) {	
     drive(speed * SPD_L_F / MAX_SPEED, speed * SPD_R_F / MAX_SPEED);
     while (analog(left_light) < grey || analog(right_light) < grey) {
-        if (analog(left_light) > grey){
-            drive(0, speed * SPD_R_F / MAX_SPEED);
-        }
-        else if (analog(right_light) > grey){
+        if (analog(right_light) > grey){
             drive(speed * SPD_L_F / MAX_SPEED, 0);
         }
+        else if (analog(left_light) > grey){
+            drive(0, speed * SPD_R_F / MAX_SPEED);
+        }    
         else if (analog(left_light) <= grey && analog(right_light) <= grey){
             drive(speed, speed);
         }
         msleep(1);
     }
+    printf("line sensed\n");
     ao();
 }
 
@@ -196,36 +267,50 @@ void turn_right(int speed, int time){
     ao();
 }
 
+void turn_left90(){
+    mav(MOT_LEFT, -1500);
+    mav(MOT_RIGHT, 1500);
+    msleep(850);
+    ao();
+}
+
+void turn_right90(){
+    mav(MOT_LEFT, 1500);
+    mav(MOT_RIGHT, -1500);
+    msleep(625);
+    ao();
+}
+
 int main(){
     //wallaby starts in the left corner against the back with claw closed facing towards create
     //servo starting positions
     set_servo_position(claw, claw_start);
     set_servo_position(arm, arm_start);
+    int change = gyrocalibrate();
     enable_servos();
     msleep(1000);
     shut_down_in(119);
     //people and ambulance go on ground of nonburning building, medical supplies go on top of nonburning building
     //firetruck goes on ground of burning building, firefighter goes on top of burning building 
-    backward(40); //back outs of starting box
+    move(-100, 650, change); //back outs of starting box
     msleep(500);
-    turn_left(1500, 800); //turn left 90 degrees 
-    forward(20);
+    turn_left90(); //turn left 90 degrees 
+    move(100, 210, change);
     set_servo_position(arm, arm_down);
     msleep(500);
     slow_servo(claw, claw_open_firetruck);
-    turn_left(1500, 800);
-    forward(30);
+    turn_left90(); //turns left 90 degrees 
+    move(100, 200, change);
     slow_servo(claw, claw_close);
-    turn_right(1500, 800); //turns right towards black line in front of building
+    line_sense(1000);
+    turn_right90(); //turns right 90 towards black line in front of building
     msleep(500);
-    line_sense_grey(1000); //goes forward until it senses the middle line 
-    backward(10);
-    msleep(500);
+    move(100, 200, change);
     scanBuilding(); //scans the first building to see where burning one is 
     msleep(500);
     if(burning_building == 0){ //burning building is close 
-        turn_left(1500, 800); 
-        forward(10); //turns and moves toward burning building 
+        turn_left90(); 
+        forward(5); //turns left 90 and moves toward burning building 
         msleep(500);
         slow_servo(claw, claw_open_firetruck); //drops off firetruck 
         msleep(500);
@@ -236,16 +321,16 @@ int main(){
     }
     else if(burning_building == 1){ //burning building is further 
         forward(40);
-        turn_left(1500, 800); //moves forward and turns toward burning building
-        forward(10);  //moves toward burning building 
+        turn_left90(); //moves forward and turns 90 toward burning building
+        forward(5);  //moves toward burning building 
         msleep(500);
         slow_servo(claw, claw_open_firetruck); //drops off firetruck 
         msleep(500);
         backward(10); 
-        turn_right(1500, 800); //backs out from building and turns 90
+        turn_right90(); //backs out from building and turns 90
         msleep(500);
         backward(30); 
-        turn_right(1500, 800); //goes back down to and turns toward medical supplies 
+        turn_right90(); //goes back down to and turns 90 toward medical supplies 
         msleep(500);
     }
     set_servo_position(arm, arm_down);
